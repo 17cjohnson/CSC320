57cb26dd1c5f87440f5dcae0b0b71f416759cfcd
17cjohnson <christian.johnson@csuglobal.edu>
2025-11-16 08:32:56 -0800

    Add files via upload

---
+++ Restaurant.java
@@ -0,0 +1,11 @@
+package Restaurants;
+
+public class Restaurant {
+
+public static void main(String[] arg) {
+    System.out.println("The Friendly Tavern");
+    System.out.println("2632 University Ave");
+    System.out.println("San Diego, CA 92104");
+	}
+
+}
